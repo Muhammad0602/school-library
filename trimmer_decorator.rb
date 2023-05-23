a4b7911205b85f666bc84604eecc1ee0cc4473f5
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-05-24 02:08:44 +0500

    Fix linters

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,11 +1,11 @@
-require "./decorator.rb"
+require './decorator'
 
 class TrimmerDecorator < Decorator
-    def correct_name
-      if  @nameable.correct_name.length < 10
-        @nameable.correct_name
-      else
-        @nameable.correct_name.slice(0, 10)
-      end
+  def correct_name
+    if @nameable.correct_name.length < 10
+      @nameable.correct_name
+    else
+      @nameable.correct_name.slice(0, 10)
     end
+  end
 end
